583336fdc8b80ec14ab48aae39a9ef00c4b1279f
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2024-11-12 13:15:15 +0300

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./PagesInMain/Home";
+import NewContact from "./PagesInMain/NewContact"; 
+import UserProfile from "./PagesInMain/Profile"; 
+import Navbar from './components/Navbar';
+import "./App.css";
+
+function App() {
+
+  return (
+
+        <Router>
+
+              <div>
+
+              <h3>Purple Chat-Space</h3>
+
+             {/* Render the Navbar */}
+        <Navbar />
+        
+        {/* Define the Routes */}
+        <Routes>
+
+          <Route path='/' element={<Home />} />
+          <Route path='/new-contact' element={<NewContact />} />
+          <Route path='/profile' element={<UserProfile />} /> 
+
+            </Routes>
+
+        </div>
+
+    </Router>
+  );
+}
 
 export default App;
+
+
+
---
+++ src/PagesInMain/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function UserProfile () {
+
+    return (
+
+        <div>
+            
+            {/* <h3>User_Profile</h3>
+            <p>Lorem ipsum......</p> */}
+            <h3>User Profile</h3>
+            <h4>Set_Up your user profile</h4>
+
+            <button>Update</button>
+
+            
+        </div>
+    );
+};
+
+export default UserProfile;
+
